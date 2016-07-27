502db0e2813d27db9380872b82543f53f28e3e7e
xJeliKx <jelikteam@gmail.com>
2016-07-27 18:33:00 +0300

    files

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+$(window).load(function(){
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+		$('body').addClass('ios');
+	};
+	$('body').removeClass('loaded'); 
+});
+function viewport(){
+	var e = window, 
+		a = 'inner';
+	if ( !( 'innerWidth' in window ) )
+	{
+		a = 'client';
+		e = document.documentElement || document.body;
+	}
+	return { width : e[ a+'Width' ] , height : e[ a+'Height' ] }
+};
+$(function(){
+
+	$('.button-nav').click(function(){
+		$(this).toggleClass('active'), 
+		$('.main-nav-list').slideToggle(); 
+		return false;
+	});
+	
+
+});
+
+var handler = function(){
+	
+	var height_footer = $('footer').height();	
+	var height_header = $('header').height();	
+	$('.content').css({'padding-bottom':height_footer+40, 'padding-top':height_header+40});
+	
+	
+	var viewport_wid = viewport().width;
+	
+	if (viewport_wid <= 991) {
+		
+	}
+	
+}
+$(window).bind('load', handler);
+$(window).bind('resize', handler);
+
+
+
